189c4c143a54a79cd22570e3276c7c97d0dd569c
Ashish Garg <01ashishgarg@gmail.com>
2018-03-02 00:10:39 +0530

    Contact us page added

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   devise_for :users
   ActiveAdmin.routes(self)
 
-  get 'home/welcome'
+  get 'welcome', to: 'home#welcome'
+  get 'contact-us', to: 'home#contact', as: 'contact_us'
   root 'home#welcome'
   resources :items, only: [:show, :index]
 end
