112e25d509bbfc30ddbd440b3dd0f1fac640155f
Ashish Garg <01ashishgarg@gmail.com>
2018-03-01 12:21:47 +0530

    active admin added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
+
+  ActiveAdmin.routes(self)
 end
